3daa939a71d812c3fbe03128dc65ca8c9192c746
olepark <oleparh@gmail.com>
2016-02-28 16:52:47 +0300

    page file test refactored

--- src/test/java/rtree/persistent/PageFileTest.java
+++ src/test/java/rtree/persistent/PageFileTest.java
@@ -1,11 +1,10 @@
 package rtree.persistent;
 
-import org.assertj.core.api.AbstractCharSequenceAssert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -22,28 +21,47 @@ public class PageFileTest {
 
   @Test
   public void testPageFile() {
-    int numOfPages = 5;
+    int numOfPages = 20;
     List<PageId> ids = IntStream.range(0, numOfPages)
         .mapToObj(i -> pageFile.newPage())
         .collect(Collectors.toList());
     int numOfPointers = (int) ids.stream().mapToLong(PageId::pageIndex).distinct().count();
     assertThat(numOfPointers).isEqualTo(ids.size());
-    long numOfActualPages = ids.stream()
+    int numOfActualPages = (int) modifyAndCount(ids);
+    assertThat(numOfActualPages).isEqualTo(numOfPages);
+
+    PageId idToErase = ids.get(2);
+    pageFile.getById(idToErase).erase();
+    List<PageId> newIds = deleteNullIds(ids);
+    int numAfterOneErased = newIds.size();
+    assertThat(numAfterOneErased).isEqualTo(numOfPages - 1);
+    assertThat(pageFile.getById(idToErase)).isNull();
+
+    int numAfterModified = (int) modifyAndCount(newIds);
+    assertThat(numAfterModified).isEqualTo(numAfterOneErased);
+
+    PageId newPage = pageFile.newPage();
+    assertThat(newPage.pageIndex()).isEqualTo(idToErase.pageIndex());
+    ZonedDateTime now = ZonedDateTime.now();
+    pageFile.getById(newPage).writeByHeader("time", now);
+    ZonedDateTime time = pageFile.getById(newPage).getByHeader("time");
+    assertThat(time).isEqualTo(now);
+  }
+
+  private List<PageId> deleteNullIds(List<PageId> ids) {
+    return ids.stream()
+          .filter(id -> pageFile.getById(id) != null)
+          .collect(Collectors.toList());
+  }
+
+  private long modifyAndCount(List<PageId> ids) {
+    return ids.stream()
         .map(pageFile::getById)
         .peek(page -> page.writeByHeader("testHeader1", "testContent1"))
         .peek(page -> page.writeByHeader("testHeader2", "testContent2"))
         .peek(page -> checkPageHeader(page, "testHeader1", "testContent1"))
         .peek(page -> checkPageHeader(page, "testHeader2", "testContent2"))
         .count();
-    assertThat(numOfActualPages).isEqualTo(numOfPages);
-    PageId idToErase = ids.get(2);
-    pageFile.getById(idToErase).erase();
-    int numAfterOneErased = (int) ids.stream()
-        .map(pageFile::getById)
-        .filter(page -> page != null)
-        .count();
-    assertThat(numAfterOneErased).isEqualTo(numOfPages - 1);
-    assertThat(pageFile.getById(idToErase)).isNull();
   }
 
   private void checkPageHeader(Page page, String header, String content) {
